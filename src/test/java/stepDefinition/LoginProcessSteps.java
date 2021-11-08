e3bd41b224c609edf7026cc98eef1e734afd79e6
Swarnendu <swarna2004@gmail.com>
2021-11-08 12:41:28 +0530

    changes in loginprocess stepdef file

--- src/test/java/stepDefinition/LoginProcessSteps.java
+++ src/test/java/stepDefinition/LoginProcessSteps.java
@@ -13,26 +13,25 @@ import pages.MyAccount;
  * @author Swarnendu
  *
  */
-public class LoginProcessSteps extends TestBase{
-	
-	MyAccount myaccount;
-	
+public class LoginProcessSteps extends TestBase {
+
 	/**
-	 * This function verifies the index page and login page titles and perform login operation	
+	 * This function verifies the index page and login page titles and perform login
+	 * operation
+	 * 
 	 * @throws Throwable
 	 */
-	
+
 	@Given("^user opens the site and logged in$")
 	public void login_Process() throws Throwable {
-	    
 		new HomePage(driver);
 		String index_page_title = HomePage.return_Landing_Page_Title();
 		assertEquals(index_page_title, prop.getProperty("Home_page_expected_title"));
 		HomePage.open_Login_Page();
-		String login_page_title=LoginPage.return_Login_Page_Title();
-		assertEquals(login_page_title,prop.getProperty("login_page_expected_title"));
+		String login_page_title = LoginPage.return_Login_Page_Title();
+		assertEquals(login_page_title, prop.getProperty("login_page_expected_title"));
 		LoginPage.login(prop.getProperty("Username"), prop.getProperty("Password"));
-	    
+
 	}
 
 }
